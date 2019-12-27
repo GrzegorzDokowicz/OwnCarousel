1961338ed789b7971b1463a8473e9807c39e535f
Grzegorz Dokowicz <grzegorz.dokowicz@gmail.com>
2019-12-27 11:03:28 +0100

    Add nextButton events, change classes between divs

--- js/controller.js
+++ js/controller.js
@@ -1,5 +1,12 @@
+/**1. zapisz klasy jako stale
+ * 2. doczytaj o Object.assign oraz wprowadz object options.
+ */
+
 function Carousel($element) {
   this.element = $element;
+  this.primaryClassName = 'primary';
+  this.recentClassName = 'recent';
+  this.comingClassName = 'coming';
   this.slides = $(this.element).children('.carousel__wrapper');
   this.setIntialClasses();
   this.attachButtonsEvents();
@@ -10,13 +17,22 @@ function Carousel($element) {
  */
 Carousel.prototype.setIntialClasses = function() {
   let slides = this.slides;
-  let lastEl = this.slides.length - 1;
-  $(slides[0]).addClass('primary');
-  $(slides[1]).addClass('coming');
-  $(slides[lastEl]).addClass('recent');
+  let lastElementKey = this.slides.length - 1;
+  $(slides[0]).addClass(this.primaryClassName);
+  $(slides[1]).addClass(this.comingClassName);
+  $(slides[lastElementKey]).addClass(this.recentClassName);
 };
+Carousel.prototype.setNewClasses = function(object) {
+  let slides = this.slides;
+  $(slides[object.primary]).addClass(this.primaryClassName);
+  $(slides[object.coming]).addClass(this.comingClassName);
+  $(slides[object.recent]).addClass(this.recentClassName);
+};
+
 Carousel.prototype.removeAllClasses = function() {
-  return $(this.slides).removeClass('recent primary coming');
+  return $(this.slides).removeClass(
+    `${this.recentClassName} ${this.primaryClassName} ${this.comingClassName}`
+  );
 };
 
 Carousel.prototype.attachButtonsEvents = function() {
@@ -25,20 +41,47 @@ Carousel.prototype.attachButtonsEvents = function() {
   $(nextButton).on('click', this.onNextButtonClick());
 };
 
+Carousel.prototype.getClassesPositions = function() {
+  let self = this;
+  let slides = self.slides;
+  let primaryElementPosition = $(slides).index(
+    $(slides).filter(`.${self.primaryClassName}`)
+  );
+  let recentElementPosition = $(slides).index(
+    $(slides).filter(`.${self.recentClassName}`)
+  );
+  let comingElementPosition = $(slides).index(
+    $(slides).filter(`.${self.comingClassName}`)
+  );
+  return {
+    primary: primaryElementPosition,
+    recent: recentElementPosition,
+    coming: comingElementPosition
+  };
+};
+
+Carousel.prototype.moveForward = function() {
+  let self = this;
+  let lastArrayElementKey = this.slides.length - 1;
+  let classesBeginingPositions = self.getClassesPositions();
+  let classesEndingPositions = Object.assign({}, classesBeginingPositions);
+  let classesEndingPositionsKeys = Object.keys(
+    classesBeginingPositions
+  ).forEach(function(key) {
+    if (classesBeginingPositions[key] === lastArrayElementKey) {
+      return (classesEndingPositions[key] = 0);
+    } else return classesEndingPositions[key]++;
+  });
+
+  return classesEndingPositions;
+};
+
 Carousel.prototype.onNextButtonClick = function() {
   let self = this;
   return function() {
-    let slides = self.slides;
-    let primaryElementPosition = 0;
-
-    for (index = 0; index < slides.length; index++) {
-      if ($(slides[index]).hasClass('primary')) {
-        primaryElementPosition = index;
-      }
-    }
+    let newClasses = self.moveForward();
     self.removeAllClasses();
-    console.log(primaryElementPosition);
-    console.log('click');
+    self.setNewClasses(newClasses);
   };
 };
 
